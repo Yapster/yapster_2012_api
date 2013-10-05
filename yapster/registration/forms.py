f1fafff49f8d5be6c8d9a2c695dcd2dd43a70396
xin <xin@k29.(none)>
2013-10-05 19:01:43 -0400

    git ignore

--- yapster/registration/forms.py
+++ yapster/registration/forms.py
@@ -8,7 +8,7 @@ you're using a custom model.
 
 """
 
-
+import hashlib
 from django.contrib.auth.models import User
 from django import forms
 from django.utils.translation import ugettext_lazy as _
@@ -29,27 +29,12 @@ class RegistrationForm(forms.Form):
     """
     required_css_class = 'required'
     
-    username = forms.RegexField(regex=r'^[\w.@+-]+$',
-                                max_length=30,
-                                label=_("Username"),
-                                error_messages={'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
     email = forms.EmailField(label=_("E-mail"))
     password1 = forms.CharField(widget=forms.PasswordInput,
                                 label=_("Password"))
     password2 = forms.CharField(widget=forms.PasswordInput,
                                 label=_("Password (again)"))
     
-    def clean_username(self):
-        """
-        Validate that the username is alphanumeric and is not already
-        in use.
-        
-        """
-        existing = User.objects.filter(username__iexact=self.cleaned_data['username'])
-        if existing.exists():
-            raise forms.ValidationError(_("A user with that username already exists."))
-        else:
-            return self.cleaned_data['username']
 
     def clean(self):
         """
@@ -59,6 +44,9 @@ class RegistrationForm(forms.Form):
         field.
         
         """
+        self.cleaned_data['username'] = hashlib.md5(
+            self.cleaned_data['email']).digest().encode('base64')[:-1]
+
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
                 raise forms.ValidationError(_("The two password fields didn't match."))
