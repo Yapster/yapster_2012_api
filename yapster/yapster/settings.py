c61093b8b6f0f56b8cc0c5d153712d46c8ece769
xin <xin@k29.(none)>
2013-10-09 21:07:26 -0400

    userinfo

--- yapster/yapster/settings.py
+++ yapster/yapster/settings.py
@@ -21,7 +21,7 @@ DATABASES = {
         'PASSWORD': '123456',
         # Empty for localhost through domain sockets or '127.0.0.1' for
         # localhost through TCP.
-        'HOST': 'localhost',
+        'HOST': 'localhost1',
         'PORT': '',                      # Set to empty string for default.
     }
 }
